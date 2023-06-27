8bf5df8516d25cd0302887be11e49cc34cd51de1
powman <paulophpweb@gmail.com>
2023-06-27 15:59:19 -0300

    Update

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Diddy Kong | USDT Rewards | Renounced | Low Tax';
 
 export default function Layout({
   children,
